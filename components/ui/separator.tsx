4aadd0fd52af90f5b0a3cfef994020b17b3eb121
Joao-Matheus-Amorim <joaomatheus.lab@gmail.com>
2026-05-19 06:01:07 -0300

    Add separator UI primitive (#264)

---
+++ components/ui/separator.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+type SeparatorProps = React.ComponentProps<"div"> & {
+  orientation?: "horizontal" | "vertical";
+  decorative?: boolean;
+};
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: SeparatorProps) {
+  return (
+    <div
+      data-slot="separator"
+      data-orientation={orientation}
+      role={decorative ? "none" : "separator"}
+      aria-orientation={decorative ? undefined : orientation}
+      className={cn(
+        "shrink-0 bg-white/10",
+        orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Separator };
