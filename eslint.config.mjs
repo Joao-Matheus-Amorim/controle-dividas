0acb277416216ee99e757c73c1d1f2588dc95fdb
Joao-Matheus-Amorim <joaomatheus.lab@gmail.com>
2026-05-23 21:23:20 -0300

    Align Next ESLint tooling versions (#547)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import { globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
-  {
-    ignores: [
-      ".next/**",
-      "node_modules/**",
-      "out/**",
-      "dist/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextVitals,
+  ...nextTypescript,
+  globalIgnores([
+    ".next/**",
+    "node_modules/**",
+    "out/**",
+    "dist/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
 ];
 
 export default eslintConfig;
